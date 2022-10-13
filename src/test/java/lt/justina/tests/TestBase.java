f00a4859f566392b1b0e8b89aaf0068617da5dfb
jpali <j.paliusyte@gmail.com>
2022-10-13 16:53:59 +0300

    Added @BeforeMethod

--- src/test/java/lt/justina/tests/TestBase.java
+++ src/test/java/lt/justina/tests/TestBase.java
@@ -1,4 +1,23 @@
 package lt.justina.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lt.justina.pages.Common;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestBase {
+
+    @BeforeMethod
+public void setUp(){
+        Common.setUpDriver();
+}
+
+
+
+    @AfterMethod
+public void close(){
+
+}
+
 }
