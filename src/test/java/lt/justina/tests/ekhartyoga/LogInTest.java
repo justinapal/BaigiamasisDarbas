75b11ead55ae0994de996d827a3469f218afc9de
jpali <j.paliusyte@gmail.com>
2022-10-14 14:55:11 +0300

    Created test method testLogInInvalidInput(){}

--- src/test/java/lt/justina/tests/ekhartyoga/LogInTest.java
+++ src/test/java/lt/justina/tests/ekhartyoga/LogInTest.java
@@ -1,10 +1,12 @@
-package lt.justina.tests;
+package lt.justina.tests.ekhartyoga;
 
 import lt.justina.pages.ekhartyoga.HomePage;
 import lt.justina.pages.ekhartyoga.LogInPage;
+import lt.justina.tests.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class LogInTest extends TestBase {
     @Override
@@ -31,5 +33,23 @@ public class LogInTest extends TestBase {
         actualUsernameGreetingMessage = HomePage.readUsernameGreetingMessage();
 
         Assert.assertEquals(actualUsernameGreetingMessage, expectedUsernameGreetingMessage);
+    }
+
+    @Test
+    private void testLogInInvalidInput(){
+        String emailInputInvalid = "t*op.mail.com";
+        String passwordInput = "insane1169";
+
+        HomePage.clickSignInButton();
+        LogInPage.enterInputEmail(emailInputInvalid);
+        LogInPage.enterInputPassword(passwordInput);
+        LogInPage.clickSignInButtonAfterCompletedInputFields();
+
+        String expectedMessage = "Invalid username or password.";
+        String actualMessage = "";
+
+        actualMessage = LogInPage.readMessageInvalidInput();
+
+        Assert.assertEquals(expectedMessage,actualMessage);
     }
 }
