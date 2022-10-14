680a484a0f253efe515733a0ea485b05fbbd804d
jpali <j.paliusyte@gmail.com>
2022-10-14 06:29:34 +0300

    Added open(String url) method

--- src/test/java/lt/justina/pages/ekhartyoga/LogInPage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/LogInPage.java
@@ -1,4 +1,9 @@
 package lt.justina.pages.ekhartyoga;
 
+import lt.justina.pages.Common;
+
 public class LogInPage {
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
 }
