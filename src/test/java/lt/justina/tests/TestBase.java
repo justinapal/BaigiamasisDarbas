540d2f37560858c72bc4a3cb91d3a4255caaf8c9
jpali <j.paliusyte@gmail.com>
2022-10-14 05:59:15 +0300

    Added TestListener to Testbase

--- src/test/java/lt/justina/tests/TestBase.java
+++ src/test/java/lt/justina/tests/TestBase.java
@@ -2,10 +2,13 @@ package lt.justina.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lt.justina.pages.Common;
+import lt.justina.utils.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
