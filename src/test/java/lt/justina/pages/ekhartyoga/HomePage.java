2b3f5f4c307e55f612778830bdac12a47012d872
jpali <j.paliusyte@gmail.com>
2022-10-14 07:16:21 +0300

    Added clickSignInButton(){} method

--- src/test/java/lt/justina/pages/ekhartyoga/HomePage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/HomePage.java
@@ -1,9 +1,14 @@
 package lt.justina.pages.ekhartyoga;
 
 import lt.justina.pages.Common;
+import lt.justina.pages.Locators;
 
 public class HomePage {
     public static void open(String url) {
         Common.openUrl(url);
+    }
+
+    public static void clickSignInButton() {
+        Common.clickElement(Locators.EkhartYoga.Home.linkSignIn);
     }
 }
