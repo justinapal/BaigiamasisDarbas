2f401f390703829ee4dfbbfcf6c92b7d8cf8e4f6
jpali <j.paliusyte@gmail.com>
2022-10-14 07:17:33 +0300

    Added class EkhartYoga with inner class Home & linkSignIn locator

--- src/test/java/lt/justina/pages/Locators.java
+++ src/test/java/lt/justina/pages/Locators.java
@@ -1,4 +1,15 @@
 package lt.justina.pages;
 
+import org.openqa.selenium.By;
+
 public class Locators {
+
+    public class EkhartYoga {
+
+        public class Home {
+
+            public static By linkSignIn = By.xpath("(//a[text()='Sign in' and @href='/log-in'])[1]");
+        }
+    }
 }
+
