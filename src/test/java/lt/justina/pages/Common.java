879809aa0f5f0029812eea53017b9f3dee631143
jpali <j.paliusyte@gmail.com>
2022-10-14 07:51:57 +0300

    Added new getElementText method

--- src/test/java/lt/justina/pages/Common.java
+++ src/test/java/lt/justina/pages/Common.java
@@ -6,23 +6,31 @@ import org.openqa.selenium.WebElement;
 
 public class Common {
 
-    public static void setUpDriver(){
+    public static void setUpDriver() {
         Driver.setDriver();
     }
 
-    public static void closeDriver() {Driver.close();
+    public static void closeDriver() {
+        Driver.close();
     }
 
-    public static void openUrl(String url) {Driver.getDriver().get(url);
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
     }
 
-    public static WebElement getElement(By locator){
+    public static WebElement getElement(By locator) {
         return Driver.getDriver().findElement(locator);
     }
+
     public static void clickElement(By locator) {
         getElement(locator).click();
     }
 
-    public static void sendKeysToElement(String keys, By locator) {getElement(locator).sendKeys();
+    public static void sendKeysToElement(String keys, By locator) {
+        getElement(locator).sendKeys();
+    }
+
+    public static String getElementText(By locator) {
+       return getElement(locator).getText();
     }
 }
