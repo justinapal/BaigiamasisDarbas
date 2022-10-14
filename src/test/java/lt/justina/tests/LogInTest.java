c77b43bfaa0b4f444a660bab7555f6fef605ddc1
jpali <j.paliusyte@gmail.com>
2022-10-14 08:31:38 +0300

    Completed test tesLogInValidInput(){}

--- src/test/java/lt/justina/tests/LogInTest.java
+++ src/test/java/lt/justina/tests/LogInTest.java
@@ -1,16 +1,35 @@
 package lt.justina.tests;
 
+import lt.justina.pages.ekhartyoga.HomePage;
 import lt.justina.pages.ekhartyoga.LogInPage;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Test;
 
 public class LogInTest extends TestBase {
     @Override
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         super.setUp();
-        LogInPage.open("https://www.ekhartyoga.com/");
+        HomePage.open("https://www.ekhartyoga.com/");
+
     }
 
+    @Test
+    private void testLogInValidInput() {
+        String emailInput = "pal.justina@gmail.com";
+        String passwordInput = "insane1169";
 
+        HomePage.clickSignInButton();
+        LogInPage.enterInputEmail(emailInput);
+        LogInPage.enterInputPassword(passwordInput);
+        LogInPage.clickSignInButtonAfterCompletedInputFields();
+
+        String expectedUsernameGreetingMessage = "Namaste, Justina";
+        String actualUsernameGreetingMessage = "";
+
+        actualUsernameGreetingMessage = HomePage.readUsernameGreetingMessage();
+
+        Assert.assertEquals(actualUsernameGreetingMessage, expectedUsernameGreetingMessage);
+    }
 }
