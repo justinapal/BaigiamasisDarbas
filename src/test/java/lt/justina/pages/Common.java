febb24fb818095f592f75b272e9960030884e411
jpali <j.paliusyte@gmail.com>
2022-10-14 06:53:55 +0300

    Added getElement(By locator){} & clickElement(By locator) methods

--- src/test/java/lt/justina/pages/Common.java
+++ src/test/java/lt/justina/pages/Common.java
@@ -1,6 +1,8 @@
 package lt.justina.pages;
 
 import lt.justina.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Common {
 
@@ -12,5 +14,12 @@ public class Common {
     }
 
     public static void openUrl(String url) {Driver.getDriver().get(url);
+    }
+
+    public static WebElement getElement(By locator){
+        return Driver.getDriver().findElement(locator);
+    }
+    public static void clickElement(By locator) {
+        getElement(locator).click();
     }
 }
