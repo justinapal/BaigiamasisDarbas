dfe0c6466646346155bbb66508dc9c56982688f6
jpali <j.paliusyte@gmail.com>
2022-10-14 06:28:56 +0300

    Set up BeforeMethod to open www.ekhartyoga.com

--- src/test/java/lt/justina/tests/LogInTest.java
+++ src/test/java/lt/justina/tests/LogInTest.java
@@ -1,4 +1,16 @@
 package lt.justina.tests;
 
-public class LogInTest {
+import lt.justina.pages.ekhartyoga.LogInPage;
+import org.testng.annotations.BeforeMethod;
+
+
+public class LogInTest extends TestBase {
+    @Override
+    @BeforeMethod
+    public void setUp(){
+        super.setUp();
+        LogInPage.open("https://www.ekhartyoga.com/");
+    }
+
+
 }
