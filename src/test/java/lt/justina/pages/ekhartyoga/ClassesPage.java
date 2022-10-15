8076e2f177da8854653004e06db05e36b9a2c3b9
jpali <j.paliusyte@gmail.com>
2022-10-15 08:26:56 +0300

    Added clickElementToGetClassesOptions(){} method

--- src/test/java/lt/justina/pages/ekhartyoga/ClassesPage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/ClassesPage.java
@@ -1,4 +1,12 @@
 package lt.justina.pages.ekhartyoga;
 
+import lt.justina.pages.Common;
+import lt.justina.pages.Locators;
+
 public class ClassesPage {
+    public static void clickElementToGetClassesOptions() {
+        Common.clickElement(
+                Locators.EkhartYoga.Classes.optionsButtonDropDown
+        );
+    }
 }
