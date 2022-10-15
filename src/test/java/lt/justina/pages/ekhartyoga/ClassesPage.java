c81b87531c2dd99d49d3412e15853792b7a3ebf8
jpali <j.paliusyte@gmail.com>
2022-10-16 01:25:21 +0300

    Added checkIsClassPresentInFavourites(){} method

--- src/test/java/lt/justina/pages/ekhartyoga/ClassesPage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/ClassesPage.java
@@ -3,6 +3,8 @@ package lt.justina.pages.ekhartyoga;
 import lt.justina.pages.Common;
 import lt.justina.pages.Locators;
 
+import java.util.List;
+
 public class ClassesPage {
     public static void clickElementToGetClassesOptions() {
         Common.clickElement(
@@ -16,5 +18,14 @@ public class ClassesPage {
 
     public static void clickElementFavourites() {
         Common.clickElement(Locators.EkhartYoga.Classes.linkFavourites);
+    }
+
+    public static boolean checkIsClassPresentInFavourites(boolean isClassPresent) {
+        List<Boolean> presenceList = Common.getStatusIfChosenElementsAreDisplayed(Locators.EkhartYoga.Classes.linkClassInFavourites);
+
+        for (boolean presence : presenceList) {
+            if (isClassPresent != presence) return false;
+        }
+        return true;
     }
 }
