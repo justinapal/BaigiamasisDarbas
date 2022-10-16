90c08901a3c13f374676c1351850bb7555e6d52b
jpali <j.paliusyte@gmail.com>
2022-10-16 14:19:17 +0300

    Added clickDropDownInputFeeling(){} method

--- src/test/java/lt/justina/pages/ekhartyoga/PlaylistsPage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/PlaylistsPage.java
@@ -1,6 +1,11 @@
 package lt.justina.pages.ekhartyoga;
 
+import lt.justina.pages.Common;
+import lt.justina.pages.Locators;
 import lt.justina.tests.TestBase;
 
 public class PlaylistsPage extends TestBase {
+    public static void clickDropDownInputFeeling() {
+        Common.clickElement(Locators.EkhartYoga.Playlists.divInputFeeling);
+    }
 }
