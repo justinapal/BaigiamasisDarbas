e259cdbe14ab55c52127bd1cfe4caf2b77b32682
jpali <j.paliusyte@gmail.com>
2022-10-17 01:57:40 +0300

    Added checkIfPlaylistsAreFIlteredAndDisplayed(){} method

--- src/test/java/lt/justina/pages/ekhartyoga/PlaylistsPage.java
+++ src/test/java/lt/justina/pages/ekhartyoga/PlaylistsPage.java
@@ -4,6 +4,8 @@ import lt.justina.pages.Common;
 import lt.justina.pages.Locators;
 import lt.justina.tests.TestBase;
 
+import java.util.List;
+
 public class PlaylistsPage extends TestBase {
     public static void clickDropDownInputFeeling() {
         Common.clickElement(Locators.EkhartYoga.Playlists.divInputFeeling);
@@ -23,5 +25,25 @@ public class PlaylistsPage extends TestBase {
 
     public static void clickButtonApply() {
         Common.clickElement(Locators.EkhartYoga.Playlists.buttonCollapsibleApply);
+    }
+
+    public static boolean checkIfPlaylistsAreFilteredAndDisplayed(boolean arePlaylistsFiltered) {
+        try {
+            List<Boolean> filteredList = Common.getStatusIfChosenElementsAreDisplayed(
+                    Locators.EkhartYoga.Playlists.linkFilteredGroundingBeginnersPlaylists);
+            if (filteredList.size() > 0)
+                return true;
+            else {
+                return false;
+            }
+        } catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            List<Boolean> filteredList = Common.getStatusIfChosenElementsAreDisplayed(
+                    Locators.EkhartYoga.Playlists.linkFilteredGroundingBeginnersPlaylists);
+            if (filteredList.size() > 0)
+                return true;
+            else {
+                return false;
+            }
+        }
     }
 }
